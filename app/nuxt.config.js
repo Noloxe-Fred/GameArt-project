79522012865472f96bc2233ccd0848a809ed5bac
Noloxe-Fred <noloxe.web@gmail.com>
2021-04-15 18:53:51 +0200

    add load with scroll on home

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+  { src: '~/plugins/infiniteloading', ssr: false }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,15 +15,46 @@ export default Vue.extend({
     ScreenCardPublic
   },
   async asyncData(context) {
-    const screensList = await context.$strapi.find('screenshots');
+    const count = await context.$strapi.count('screenshots');
+    const screensList = await context.$strapi.find('screenshots', { _start: 0, _limit: 10 });
     return {
       screensList,
+      count
     }
   },
   data() {
     return {
       screensList: [],
+      start: 11,
+      count: null,
     }
+  },
+  methods: {
+    scroll() {
+      window.onscroll = () => {
+        console.log('listen')
+        let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+
+        if (bottomOfWindow) {
+         this.loadMoreImages();
+        }
+      }
+    },
+    increaseStart() {
+      this.start = start + 10;
+    },
+    async loadMoreImages() {
+      console.log('infinite Scroll', this.screensList.length, this.count)
+      if (this.screensList.length >= this.count) {
+        return;
+      }
+      const newScreens = await this.$strapi.find('screenshots', { _start: this.start, _limit: 10 });
+      this.screensList.push(...newScreens);
+      this.increaseStart;
+    }
+  },
+  mounted() {
+    this.scroll();
   }
 })
 </script>
