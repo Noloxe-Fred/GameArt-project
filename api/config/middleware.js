dc9f50e7d765a24d71b78f8c0eba23a926e3b69e
Noloxe-Fred <noloxe.web@gmail.com>
2021-05-13 12:58:53 +0200

    fix empty screensList & local cors

--- api/config/middleware.js
+++ api/config/middleware.js
@@ -1,10 +1,9 @@
 module.exports = {
-  //...
   settings: {
     cors: {
       enabled: true,
       headers: '*',
-      origin: ['http://54.36.101.143'],
+      origin: ['http://54.36.101.143', 'http://localhost:1337', 'http://localhost:3000'],
     },
   },
 };
