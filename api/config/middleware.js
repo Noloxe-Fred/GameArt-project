2e45b0638f549c4aed73b83056f8a0a91e1be909
Noloxe-Fred <noloxe.web@gmail.com>
2021-05-13 12:10:04 +0200

    update origin property

--- api/config/middleware.js
+++ api/config/middleware.js
@@ -4,7 +4,7 @@ module.exports = {
     cors: {
       enabled: true,
       headers: '*',
-      origin: ['http://54.36.101.143', 'http://54.36.101.143:1337'],
+      origin: ['*'],
     },
   },
 };
